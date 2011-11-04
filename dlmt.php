4f8f1cc5fa629b8985890350a88f4e06ee5822f1
Ashfame <mail@ashfame.com>
2011-11-04 06:25:42 +0530

    added is_file() check

--- dlmt.php
+++ dlmt.php
@@ -6,10 +6,12 @@ error_reporting( -1 );
 
 /**
  * Function which shows time diff in human-friendly form
+ *
+ * Credit: CSS-tricks.com
  */
 
-function time_since( $time )
-{
+function time_since( $time ) {
+	
 	$periods = array( "second", "minute", "hour", "day", "week", "month", "year", "decade" );
 	$lengths = array( "60","60","24","7","4.35","12","10" );
 
@@ -18,30 +20,41 @@ function time_since( $time )
 	$difference = $now - $time;
 	$tense = "ago";
 
-	for ( $j = 0; $difference >= $lengths[$j] && $j < count( $lengths ) - 1; $j++ ) {
+	for ( $j = 0; $difference >= $lengths[$j] && $j < count( $lengths ) - 1; $j++ )
 		$difference /= $lengths[$j];
-	}
 
 	$difference = round( $difference );
 
 	if ( $difference != 1 )
 		$periods[$j].= "s";
 	
-	return "$difference $periods[$j] 'ago' ";
+	return "$difference $periods[$j] ago";
 }
 
+
+/**
+ * Main function which does the all the work
+ */
 
 function dlmt( $path = '' ) {
 	
 	$files = scandir( $path );
+	
+	echo'<pre>';
+	print_r($files);
+	echo'</pre>';
 		
 	$timestamp = array();
 	
 	// read last modified time of all files
 	foreach ( $files as $file ) {
-		if ( $file != '.' && $file != '..' )
+		if ( $file != '.' && $file != '..' && is_file( $path . $file ) )
 			$timestamp[$file] = filemtime( $path . $file );
 	}
+	
+	echo'<pre>';
+	print_r($timestamp);
+	echo'</pre>';
 	
 	$most_fresh_file_time = max( $timestamp );
 	
